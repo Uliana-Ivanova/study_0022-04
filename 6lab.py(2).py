8bd9b64d1457d4719098b401a9e1b21fd134ab2e
Uliana-Ivanova <78957990+Uliana-Ivanova@users.noreply.github.com>
2021-03-17 14:31:39 +0300

    Update 6lab.py(2).py

--- 6lab.py(2).py
+++ 6lab.py(2).py
@@ -19,5 +19,5 @@ else:
     words1=lon.split(',')
     print("словарь")   #создаем словарь
     d={ }
-    d={words[0]:words1[0], words[1]:words1[1], words[2]:words1[2]}
+    d={words[0]:words1[0], words[1]:words1[1], words[2]:words1[2], words[3]:words1[3], words[4]:words1[4], words[5]:words1[5], words[6]:words1[6], words[7]:words1[7], words[8]:words1[8], words[9]:words1[9]}
     print(d)
